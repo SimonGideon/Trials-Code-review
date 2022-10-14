cb40bc7d9da14727391b11e709330492fcfd9b1d
Simon Gideon <61404403+SimonGideon@users.noreply.github.com>
2022-10-14 16:33:48 +0300

    Create run.js

---
+++ run.js
@@ -0,0 +1,20 @@
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+user.data.friends.push('Simon');
+console.log(user);
